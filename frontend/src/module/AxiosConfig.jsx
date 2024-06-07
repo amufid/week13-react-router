ea2eec4a809ac81dc460e3638aa4203e3b880947
amufid <91891129+amufid@users.noreply.github.com>
2024-06-07 22:28:21 +0700

    change url

--- frontend/src/module/AxiosConfig.jsx
+++ frontend/src/module/AxiosConfig.jsx
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie'
 
 // Buat instance Axios khusus
 const instance = axios.create({
-   baseURL: 'https://nodejs-prisma-d9uyk4trh-mufids-projects-16b66c7d.vercel.app/?vercelToolbarCode=6y5IDxh5Vdsf9Nm'
+   baseURL: 'https://nodejs-prisma-d9uyk4trh-mufids-projects-16b66c7d.vercel.app'
 });
 
 // Tambahkan interceptor request untuk memasukkan token ke header
