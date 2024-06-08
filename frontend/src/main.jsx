afa6a1423f55df706dab7fa14642c20215cf72b9
amufid <91891129+amufid@users.noreply.github.com>
2024-06-08 14:15:24 +0700

    update-2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '@fontsource/raleway/400.css'
 import '@fontsource/open-sans/700.css'
 import theme from './theme.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="<173520146791-0r2h3b5v39iajnb091tuv6bjldm02iqo.apps.googleusercontent.com>">
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </GoogleOAuthProvider>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
