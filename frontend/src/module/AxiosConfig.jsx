68947b057e743e2dcede652fd0359adc133d3893
amufid <91891129+amufid@users.noreply.github.com>
2024-06-08 13:43:20 +0700

    update code

--- frontend/src/module/AxiosConfig.jsx
+++ frontend/src/module/AxiosConfig.jsx
@@ -1,9 +1,40 @@
+// import axios from 'axios';
+// import Cookies from 'js-cookie'
+
+// // Buat instance Axios khusus
+// const instance = axios.create({
+//    baseURL: 'https://nodejs-prisma-16n8iq9hf-mufids-projects-16b66c7d.vercel.app'
+// });
+
+// // Tambahkan interceptor request untuk memasukkan token ke header
+// instance.interceptors.request.use(
+//    (config) => {
+//       // Ambil token dari localStorage
+//       // const token = localStorage.getItem('token');
+//       const token = Cookies.get('token')
+
+//       // Jika token ada, tambahkan ke header Authorization
+//       if (token) {
+//          config.headers['Authorization'] = `Bearer ${token}`;
+//       }
+
+//       return config;
+//    },
+//    (error) => {
+//       return Promise.reject(error);
+//    }
+// );
+
+// export default instance;
+
+
 import axios from 'axios';
 import Cookies from 'js-cookie'
 
 // Buat instance Axios khusus
 const instance = axios.create({
-   baseURL: 'https://nodejs-prisma-16n8iq9hf-mufids-projects-16b66c7d.vercel.app'
+   baseURL: 'https://nodejs-prisma-16n8iq9hf-mufids-projects-16b66c7d.vercel.app',
+   headers: { 'X-Custom-Header': 'foobar' }
 });
 
 // Tambahkan interceptor request untuk memasukkan token ke header
